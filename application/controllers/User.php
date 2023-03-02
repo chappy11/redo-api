036cd3d7e1009bf3715017530d21934417eaf3ca
chappy <lumangyao112@gmail.com>
2023-03-02 23:06:52 +0800

    create user registration and login

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,8 @@
 
         public function __construct(){
             parent::__construct();
-            $this->load->model(array("User_Model","Notification_Model","UserLog_Model","Customer_Model","Shop_Model","Admin_Model",""));
+            
+            $this->load->model(array("User_Model"));
         }
         //note:
         //user role:
@@ -14,423 +15,47 @@
         //1 seller
         //2 customer
         
-        
-        public function warmup_post(){
+        public function register_post(){
             $data = $this->decode();
-            $arr = array("email"=>$data->email);
-            $this->res(1,$this->Customer_Model->checkIsEmailExist($data->email),"error",1);
-        }
-
-        public function isEmailExist($email,$type){
-            $isExist = false;
-
-            if($type === "shop" && $this->Shop_Model->checkShopEmailExist($email)){
-                $isExist = true;
-            }
-
-            if($type === "customer" && $this->Customer_Model->checkIsEmailExist($email)){
-                $isExist =  true;
-            }
-
-            return $isExist;
-
-        }
-
-        public function isMobileExist($mobile,$type){
-            $isExist = false;
-
-            if($type === "shop" && $this->Shop_Model->checkMobileExist($mobile)){
-                $isExist = true;
-            }
-
-            if($type === "customer" && $this->Customer_Model->checkMobileNumberExist($mobile)){
-                $isExist = true;
-            }
-
-            return $isExist;
-        }
-
-        //create account for customer
-        public function signupcustomer_post(){
-            $profile_picture =  $_FILES["profilePicture"]["name"];
-            $username = $this->post("username");
-            $password = $this->post("password");
-            $firstname = $this->post("firstname");
-            $middleInitial = $this->post("mi");
-            $lastname = $this->post("lastname");
-            $gender = $this->post("gender");
-            $birthdate = $this->post("birthdate");
-            $address = $this->post("address");
-            $email = $this->post("email");
-            $contact = $this->post("contact");
-
-            $isEmailExist = $this->isEmailExist($email,"customer");
-            $isMobileExist = $this->isMobileExist($contact,"customer");
-            if($isEmailExist){
-                
-                $this->res(0,null,"Your Email is Already Exist",0);
-            
-            }else if($isMobileExist){
-            
-                $this->res(0,null,"Your Mobile Number is Alreay Exist",0);
-            
-            }else{
-            
-                $userData = array(
-                    "username" => $username,
-                    "password" => md5($password),
-                    "user_roles" => 2, //customer
-                    "user_status" => 0, //status is active
-                );
-    
-                $isCreated = $this->User_Model->createUser($userData);
-                
-                if(!$isCreated){
-                  $this->res(0,null,"Something went wrong",0);
-                }else{
-                   
-                    $newUser = $this->User_Model->getNewUser(); //get latest user
-              
-                    try{
-                     
-                        $newCustomer = array(
-                            "user_id" => $newUser[0]->user_id,
-                            "email" => $email,
-                            "contact" => $contact,
-                            "firstname" => $firstname,
-                            "lastname" => $lastname,
-                            "middlename" => $middleInitial,
-                            "gender" => $gender,
-                            "birthdate" => $birthdate,
-                            "addresss" => $address,
-                            "profilePic" => "profiles/".$profile_picture
-                        );
-            
-                        $createCustomer = $this->Customer_Model->createCustomer($newCustomer);
-                    
-                        if($createCustomer){
-                            
-                            $notif_data = array(
-                                "notif_title"=>"New Registered Customer",
-                                "notif_message"=> $newUser[0]->username." was successfully register to our system please check it for approval",
-                                "notif_receiver"=> 1,
-                                "notif_link"=>"/pendinguser",
-                                "isRead"=>0
-                            );
-
-                            $isSuccessNotif = $this->Notification_Model->create($notif_data);
-                            if($isSuccessNotif){
-                                $this->res(1,null,"You Have Successfully Registered",0);
-                                move_uploaded_file($_FILES['profilePicture']['tmp_name'],"profiles/".$profile_picture);
-                            }else{
-                                $this->res(0,null,"Something went wrong",0);
-                            }
-                            
-                        }else{
-                            $this->res(0,null,"Something went wrong",0);
-                        }
-                        
-                      
-                    } 
-                    catch(Exception $e){
-                        $this->User_Model->deleteUser($newUser[0]->user_id);
-                        $this->res(0,null,"Something went wrong",0);
-                    }
-                }
-    
-            }          
-        }
-
-        //create account for shop
-        public function createshop_post(){
-            $shop_logo = $_FILES['shopLogo']['name'];
-            $username = $this->post("username");
-            $password = $this->post("password");
-            $shop_email = $this->post("shopEmail");
-            $shop_name = $this->post("shopName");
-            $shop_description = $this->post("shopDescription");
-            $firstname = $this->post("firstname");
-            $middlename = $this->post("middlename");
-            $lastname = $this->post("lastname");
-            $address = $this->post("address");
-            $shopContact = $this->post("contact");
-            $cFname = $this->post("cfname");
-            $cLname = $this->post("clname");
-            $cNumber = $this->post("cNumber"); 
-            $isEmailExist = $this->isEmailExist($shop_email,"shop");
-            $isMobileNumberExist = $this->isMobileExist($shopContact,'shop');
-
-            if($isEmailExist){
-                
-                $this->res(0,null,"This email is already exist",0);
-            }else{
-
-                $arr = array("username"=>$username);
-                $this->res(1,$this->User_Model->checkDataExist($arr),"error",1);
-            
-    
-                $userData = array(
-                    "username" => $username,
-                    "password" => md5($password),
-                    "user_roles" => 1,
-                    "user_status" => 0, 
-                );
-    
-                $isCreated = $this->User_Model->createUser($userData);
-    
-                if(!$isCreated){
-                    $this->res(0,null,"Something went wrong",0);
-                }else{
-                    $newUserData = $this->User_Model->getNewUser();
-    
-                    try{
-                        $newShop = array(
-                            "user_id" => $newUserData[0]->user_id,
-                            "logo" => "shops/".$shop_logo,
-                            "shopName" => $shop_name,
-                            "shopEmail" => $shop_email,
-                            "shopDescription" => $shop_description,
-                            "ownerFirstname" => $firstname,
-                            "ownerMiddlename" => $middlename,
-                            "ownerLastname" => $lastname,
-                            "shopAddress" => $address,
-                            "subscription_id" => 0,
-                            "shopContact" => $shopContact,
-                            "contactPersonNumber"=>$cNumber,
-                            "contactPersonFname"=>$cFname,
-                            "contactPersonLname"=>$cLname
-                        );
-        
-                        $isShopCreated = $this->Shop_Model->createShop($newShop);
-        
-                        if(!$isShopCreated){
-                            $this->res(0,null,"Something went wrong, Sorry for Inconvinience",0);
-                        }
-
-                        $notif_data = array(
-                            "notif_title"=>"New Registered Seller",
-                            "notif_message"=> $newUserData[0]->username." was successfully register to our system please check it for approval",
-                            "notif_receiver"=> 1,
-                            "notif_link"=>"/pendingshops",
-                            "isRead"=>0
-                        );
-
-                        $this->Notification_Model->create($notif_data);
-
-                        $this->res(1,null,$shop_name." is successfully created!",0);
-                        move_uploaded_file($_FILES['shopLogo']['tmp_name'],"shops/".$shop_logo);
-               
-                    }catch(Exception $e){
-                        $this->User_Model->deleteUser($newUserData[0]->user_id);
-                        $this->res(0,null,"Something went wrong",0);
-                    }
-                }
-
-            }
-
-
-
-      
-        }
-        
-        //login
-        public function login_post(){
-            $data = $this->decode();
-            $username = isset($data->username) ? $data->username : "";
-            $password = isset($data->password) ? $data->password : "";
-            $browserName = isset($data->browserName) ? $data->browserName : "";
-       
-            $user = $this->User_Model->login($username,md5($password));
-                            
-                if(count($user) < 1){
-                    $this->res(0,null,"Invalid account please check your username or password",0);
-                }else{
-                    $pay = array(
-                        "user_id"=>$user[0]->user_id,
-                        "browserName" => $browserName,
-                    );
-
-                    if($user[0]->user_status === "1"){
-                        $this->UserLog_Model->insert($pay);
-                    }
-
-                   
-                    if($user[0]->user_status === "0"){
-                        $this->res(0,null,"Your Account is Inactive",0);
-                    }
-                    else if($user[0]->user_roles == "1"){
-                        $shopData = $this->Shop_Model->getShopByUserId($user[0]->user_id);
-                    
-                        $this->res(1,$shopData[0],"Successfully Login",0);
-                    }
-                    else if($user[0]->user_roles == "2"){
-                        $customerData = $this->Customer_Model->getCustomerByUserId($user[0]->user_id);
-    
-                        $this->res(1,$customerData[0],"Successfully Login",0);
-                    }else if($user[0]->user_roles == 0 || $user[0]->user_roles == 3){
-                        $adminData = $this->Admin_Model->getAdminByUserId($user[0]->user_id);
-                        
-                        $this->res(1,$adminData[0],"Successfully Login",0);
-                    }
-               }              
-        }
-
-        public function getpendingcustomer_get(){
-            $data = $this->Customer_Model->getPendingCustomer();
-            if(count($data) > 0){
-                $this->res(1,$data,"Data found",count($data));
-            }else{
-                $this->res(0,null,"Data not found",0);
-            }
-        }
-
-        public function getusers_get($roles,$status){
-            $data = $this->User_Model->getUserByStatus($roles,$status);
-            $this->res(1,$data,"data found",0);
-        }
-
-        public function updatestatus_post(){
-            $data = $this->decode();
-            $user_id = $data->user_id;
-            $status = $data->status;
-
-            $userInfo = $this->User_Model->getuser($user_id)[0];
-
-            $email = "";
-
-            if($userInfo->user_roles == "1"){
-                $email = $this->Shop_Model->getShopByUserId($user_id)[0]->shopEmail;
-            }else if($userInfo->user_roles == "2"){
-                $email = $this->Customer_Model->getCustomerByUserId($user_id)[0]->email;
-            }
-
-            $header = "";
-            $message ="";
-
-            if($status == "1"){
-                $header = "Admin activated your account";
-                $message = "Congrats your account has been activated you can do buy and sell now";
-            }else{
-                $header = "Admin deactivated your account";
-                $message = "Your account has been deactivated by admin due for some reason";
-            }
-
-            $payload = array("user_status"=>$status);
-           
-            $isUpdateUser = $this->User_Model->updateUser($user_id,$payload);
-
-                if($isUpdateUser){
-                    $notif_data = array(
-                        "notif_title"=>$header,
-                        "notif_message"=> $message,
-                        "notif_receiver"=> $user_id,
-                        "notif_link"=>"/",
-                        "isRead"=>0
-                    );
-                    if($status == "1"){
-                        $this->approvedEmail($email);
-                    }
-                    $this->Notification_Model->create($notif_data);
-                    $this->res(1,null,"Succesfully Update",0);
-                }else{
-                    $this->res(0,null,"Something went wrong",0);
-                }
-         
-        }
-
-        public function getuser_get($id,$type){
-            if($type == "shop"){
-                $shop = $this->Shop_Model->getShopByUserId($id);
-                $this->res(1,$shop[0],"data found",0);
-            }else{
-                $customer = $this->Customer_Model->getCustomerByUserId($id);
-                $this->res(1,$customer[0],"Data found",0);
-            }            
-
-        }
-
-        public function getlogs_get(){
-            $data = $this->UserLog_Model->getlog();
-  
-            $this->res(1,$data,"Data not found",0);
-        }
-  
-        public function changepass_post(){
-            $data = $this->decode();
+            $fullname = $data->fullname;
+            $email = $data->email;
+            $phoneNumber = $data->phone;
             $password = $data->password;
-            $id = $data->id;
 
-            $payload = array("password"=>$password);
-        
-            $isUpdate = $this->User_Model->updateUser($id,$payload);
+            $data = array(
+                "profilePic" => 'profiles/no_user.png',
+                "fullname" => $fullname,
+                "email" => $email,
+                "password" => $password,
+                "phoneNumber" => $phoneNumber,
+                "userRoles" => "user",
+                "status" => 1
+            );
 
-            if($isUpdate){
-                $this->res(1,null,"Successfully Updating Your Password",0);
+            $response = $this->User_Model->create($data);
+
+            if($response){
+                $this->res(1,null,"Successfully Login",0);
             }else{
                 $this->res(0,null,"Something went wrong",0);
             }
-        
+
         }
 
-        public function update_post($id){
+        public function login_get(){
             $data= $this->decode();
-            $user_id = $id;
-            $user_data = $this->User_Model->getuser($user_id)[0];
-            
-            
-            if($user_data->user_roles == "2"){
-                $isCustomerUpdate = $this->Customer_Model->updateCustomerByUserId($user_id,$data);
 
-                if($isCustomerUpdate){
-                    $customer = $this->Customer_Model->getCustomerByUserId($user_id);
-                    
-                    $this->res(1,$customer[0],"Successfully Updated",0);
-                }else{
-                    $this->res(0,null,"Something went wrong",0);
-                }
-            }else if($user_data->user_roles=="1"){
-                $isShopUpdated = $this->Shop_Model->updateShopByUserId($user_id,$data);
-                
-                if($isShopUpdated){
-                    $shop = $this->Shop_Model->getShopByUserId($user_id);
-                    
-                    $this->res(1,$shop[0],"Successfully Updated",0);
-                }else{
-                    $this->res(0,null,"SOmething went wrong",0);
-                }
+            $email = $data->email;
+            $password = $data->password;
+
+            $resp = $this->User_Model->login($email,$password);
+
+            $noResp = count($resp);
+            if($noResp > 0){
+                $this->res(1,$data,"Successfully Login",$noResp);
+            }else{
+                $this->res(0,null,"Something went wrong");
             }
-        }
-
-        public function approvedEmail($email){
-            $ht ="<html>
-            <div style='margin: auto; width: 600px'>
-              <h3 style='text-align: center'>Welcome to Petsociety</h3>
-          
-              <p style='text-align: center'>
-            Congratulations! Your account has been successfully activated. You may now
-                log in to Pet Society using your registered username and password. Get
-                ready to explore the platform, buy products, and pets. Have fun!
-              </p>
-            </div>
-          </html>";
-            $config['protocol']    = 'smtp';
-            $config['smtp_host']    = 'smtp.mailtrap.io';
-            $config['smtp_port']    = '2525';
-            $config['smtp_user'] = '7ec9d17b2163b3';
-            $config['smtp_pass'] = '55c83a05d8d4cb';
-            $config['charset']    = 'utf-8';
-            $config['newline']    = "\r\n";
-            $config['mailtype'] = 'html'; // or html
-            $config['validation'] = TRUE; // bool whether to validate email or not      
-            $this->load->library('email');
-
-            $this->email->initialize($config);
-            $this->email->from("no-reply@petsoceity.com");
-            $this->email->to($email);
-            $this->email->subject("Email Verificatoin Code");
-            $this->email->message($ht);
-  
-            $this->email->send();
         }
     }
 
--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -1,7 +1,7 @@
 <?php 
 
     class User_Model extends CI_Model{
-        private $tbl = "user";
+        private $tbl = "users";
 
         public function __construct(){
             parent::__construct();
@@ -9,81 +9,22 @@
         }
 
         //create new User
-        public function createUser($user=array()){
+        public function create($user=array()){
             return $this->db->insert($this->tbl,$user);
         }
 
-        //login user
 
-        //select * from user where username='kye' && password='1234kye'
-        public function login($username,$password){
+        //login user
+        public function login($email,$password){
             $this->db->select("*");
             $this->db->from($this->tbl);
-            $this->db->where("username",$username);
+            $this->db->where("email",$email);
             $this->db->where("password",$password);
             $query = $this->db->get();
             return $query->result();
         }
 
-        public function checkUserNameExist($username){
-            $this->db->select("username");
-            $this->db->from($this->tbl);
-            $this->db->where("username",$username);
-            $query = $this->db->get();
-            if(count($query->result()) > 0){
-                return true;
-            }else{
-                return false;
-            }
-        }
-
-        public function getNewUser(){
-            $this->db->select("*");
-            $this->db->from($this->tbl);
-            $this->db->order_by("user_id","DESC");
-            $query = $this->db->get();
-            return $query->result();
-        }
-        
-        public function updateUser($id,$data){
-            return $this->db->update($this->tbl,$data,"user.user_id=".$id);
-        }
-
-        public function deleteUser($user_id){
-            return $this->db->delete($this->tbl,array("user_id"=>$user_id));
-        }
-
-        public function checkDataExist($compareData){
-            $this->db->select("*");
-            $this->db->from($this->tbl);
-            $this->db->where(array_keys($compareData),array_values($compareData));
-            $query = $this->db->get();
-            return $query->result();
-        }
-
-        public function getUserByStatus($roles,$status){
-            $this->db->select("*");
-            $this->db->from($this->tbl);
-            if($status != 3){
-                $this->db->where("user.user_status",$status);
-            }
-           
-            if($roles == 2){
-                $this->db->join("customer","customer.user_id=user.user_id");
-            }else{
-                $this->db->join("shop","shop.user_id=user.user_id");
-            }
-            $query =$this->db->get();
-            return $query->result();
-        }
-
-        public function getuser($user_id){
-            $this->db->select("*");
-            $this->db->from($this->tbl);
-            $this->db->where("user_id",$user_id);
-            $query = $this->db->get();
-            return $query->result();
-        }
+    
     }
 
 ?>
